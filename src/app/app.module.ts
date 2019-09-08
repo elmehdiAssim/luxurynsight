32a558dcebff96efb351ffaecf3bbdc85f11cb63
Elmehdi Assimeddine <elmehdiassimeddine@gmail.com>
2019-09-08 16:02:41 +0100

    chore: g c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { NewsItemComponent } from './news/news-item/news-item.component';
+import { NewsListComponent } from './news/news-list/news-list.component';
+import { NewsModalComponent } from './news/news-modal/news-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsItemComponent,
+    NewsListComponent,
+    NewsModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/news/news-modal/news-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-modal',
+  templateUrl: './news-modal.component.html',
+  styleUrls: ['./news-modal.component.scss']
+})
+export class NewsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
